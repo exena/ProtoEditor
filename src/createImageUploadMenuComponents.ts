3fa21e83cfdf3e6897dd1f60e6c8b5fd0ed3baf3
exena <exena@naver.com>
2025-11-10 18:19:28 +0900

    chore: 주석 수정

--- src/createImageUploadMenuComponents.ts
+++ src/createImageUploadMenuComponents.ts
@@ -8,7 +8,7 @@ import type { EditorView } from "prosemirror-view";
 async function uploadImageFile(file: File): Promise<string> {
   // 외부에서 업로드용 URL이 지정되어 있으면 fetch 사용
   const uploadUrl = (window as any).uploadImageUrl; // 서버에서 이미지 업로드 요청을 받는 URL(필수)
-  const uploadHeader = (window as any).uploadImageHeader; // CSRF 토큰(옵션)
+  const uploadHeader = (window as any).uploadImageHeader; // 요청 헤더(CSRF 토큰을 넣기 위한 옵션)
   const uploadRequestParam = (window as any).uploadImageRequestParam; // 서버에서 formData 안의 파일을 읽을 때 쓰는 키(필수)
   const uploadResponseKey = (window as any).uploadImageResponseKey; // JSON 응답 키(옵션)
 
